e688e39ae2ebd7916586875cdbec8f28e46081d7
David Kellum <dek-oss@gravitext.com>
2018-06-06 12:42:39 -0700

    docs(examples): Simplify client doctest with current_thread runtime

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -23,53 +23,37 @@
 //!
 //! ## Example
 //!
-//! ```no_run
+//! ```
+//! extern crate futures;
+//! extern crate tokio;
 //! extern crate hyper;
 //!
+//! use futures::{Future, Stream};
+//! use tokio::runtime::current_thread;
 //! use hyper::Client;
-//! # #[cfg(feature = "runtime")]
-//! use hyper::rt::{self, lazy, Future, Stream};
 //!
-//! # #[cfg(feature = "runtime")]
 //! fn main() {
-//!     // A runtime is needed to execute our asynchronous code.
-//!     rt::run(lazy(|| {
-//!         let client = Client::new();
+//!     let mut rt = current_thread::Runtime::new().unwrap();
+//!     let client = Client::new();
+//!     let job = client
+//!         // Make a GET /ip to 'http://httpbin.org'
+//!         .get("http://httpbin.org/ip".parse().unwrap())
 //!
-//!         client
-//!             // Make a GET /ip to 'http://httpbin.org'
-//!             .get("http://httpbin.org/ip".parse().unwrap())
+//!         // And then, if the request gets a response...
+//!         .and_then(|res| {
+//!             println!("status: {}", res.status());
 //!
-//!             // And then, if the request gets a response...
-//!             .and_then(|res| {
-//!                 println!("status: {}", res.status());
+//!             // Concatenate the body stream into a single buffer...
+//!             // This returns a new future, since we must stream body.
+//!             res.into_body().concat2()
+//!         });
 //!
-//!                 // Concatenate the body stream into a single buffer...
-//!                 // This returns a new future, since we must stream body.
-//!                 res.into_body().concat2()
-//!             })
-//!
-//!             // And then, if reading the full body succeeds...
-//!             .and_then(|body| {
-//!                 // The body is just bytes, but let's print a string...
-//!                 let s = ::std::str::from_utf8(&body)
-//!                     .expect("httpbin sends utf-8 JSON");
-//!
-//!                 println!("body: {}", s);
-//!
-//!                 // and_then requires we return a new Future, and it turns
-//!                 // out that Result is a Future that is ready immediately.
-//!                 Ok(())
-//!             })
-//!
-//!             // Map any errors that might have happened...
-//!             .map_err(|err| {
-//!                 println!("error: {}", err);
-//!             })
-//!     }));
+//!     let body = rt.block_on(job).unwrap();
+//!     // The body is just bytes, but let's print a string...
+//!     let s = ::std::str::from_utf8(&body)
+//!                 .expect("httpbin sends utf-8 JSON");
+//!     println!("body: {}", s);
 //! }
-//! # #[cfg(not(feature = "runtime"))]
-//! # fn main () {}
 //! ```
 
 use std::fmt;
