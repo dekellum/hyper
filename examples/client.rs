421ea809d6142d71ae03a8be57de1871038d3540
David Kellum <dek-oss@gravitext.com>
2018-06-06 12:17:41 -0700

    docs(examples): Demonstrate the halting problem

--- examples/client.rs
+++ examples/client.rs
@@ -26,33 +26,32 @@ fn main() {
         println!("This example only works with 'http' URLs.");
         return;
     }
+    let client = Client::new();
 
-    rt::run(rt::lazy(move || {
-        let client = Client::new();
+    let job = client
+    // Fetch the url...
+        .get(url)
+    // And then, if we get a response back...
+        .and_then(|res| {
+            println!("Response: {}", res.status());
+            println!("Headers: {:#?}", res.headers());
 
-        client
-            // Fetch the url...
-            .get(url)
-            // And then, if we get a response back...
-            .and_then(|res| {
-                println!("Response: {}", res.status());
-                println!("Headers: {:#?}", res.headers());
+            // The body is a stream, and for_each returns a new Future
+            // when the stream is finished, and calls the closure on
+            // each chunk of the body...
+            res.into_body().for_each(|chunk| {
+                io::stdout().write_all(&chunk)
+                    .map_err(|e| panic!("example expects stdout is open, error={}", e))
+            })
+        })
+    // If all good, just tell the user...
+        .map(|_| {
+            println!("\n\nDone.");
+        })
+    // If there was an error, let the user know...
+        .map_err(|err| {
+            eprintln!("Error {}", err);
+        });
 
-                // The body is a stream, and for_each returns a new Future
-                // when the stream is finished, and calls the closure on
-                // each chunk of the body...
-                res.into_body().for_each(|chunk| {
-                    io::stdout().write_all(&chunk)
-                        .map_err(|e| panic!("example expects stdout is open, error={}", e))
-                })
-            })
-            // If all good, just tell the user...
-            .map(|_| {
-                println!("\n\nDone.");
-            })
-            // If there was an error, let the user know...
-            .map_err(|err| {
-                eprintln!("Error {}", err);
-            })
-    }));
+    rt::run(job); //blocks indefinitely, because client is not yet dropped.
 }
